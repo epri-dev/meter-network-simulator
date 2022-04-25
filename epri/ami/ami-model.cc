3439b6e8cd05753ba60973ad593c044a8d938919
Ed Beroset <beroset@ieee.org>
2022-04-25 14:23:11 -0400

    Add initial AMI model using IPv6, WiFi, UDP echo
    This is an adaptation of one of the tutorial files, modified to use IPv6
    instead of IPv4 and eliminating non-RF nodes from the network.  It's a
    first step to creating a real AMI model.

---
+++ epri/ami/ami-model.cc
@@ -0,0 +1,165 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "ns3/core-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/network-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/yans-wifi-helper.h"
+#include "ns3/ipv6-list-routing-helper.h"
+#include "ns3/ssid.h"
+
+// Default Network Topology
+//
+//      Wifi 2001:1::
+//    AP
+//    *    *    *    *
+//    |    |    |    |   
+//   n0   n1   n2   n3 
+// client          server 
+
+using namespace ns3;
+
+void
+CourseChange (std::string context, Ptr<const MobilityModel> model)
+{
+  Vector position = model->GetPosition ();
+  NS_LOG_UNCOND (context << 
+    " x = " << position.x << ", y = " << position.y);
+}
+
+NS_LOG_COMPONENT_DEFINE ("EPRI_AMI");
+
+int 
+main (int argc, char *argv[])
+{
+  bool verbose = true;
+  uint32_t nWifi = 3;
+  bool tracing = false;
+
+  CommandLine cmd (__FILE__);
+  cmd.AddValue ("nWifi", "Number of wifi STA devices", nWifi);
+  cmd.AddValue ("verbose", "Tell echo applications to log if true", verbose);
+  cmd.AddValue ("tracing", "Enable pcap tracing", tracing);
+
+  cmd.Parse (argc,argv);
+
+  // The underlying restriction of 18 is due to the grid position
+  // allocator's configuration; the grid layout will exceed the
+  // bounding box if more than 18 nodes are provided.
+  if (nWifi > 18)
+    {
+      std::cout << "nWifi should be 18 or less; otherwise grid layout exceeds the bounding box" << std::endl;
+      return 1;
+    }
+
+  if (verbose)
+    {
+      LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
+      LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+    }
+
+  NodeContainer wifiStaNodes;
+  wifiStaNodes.Create (nWifi);
+  NodeContainer wifiApNode = wifiStaNodes.Get (0);
+
+  YansWifiChannelHelper channel = YansWifiChannelHelper::Default ();
+  YansWifiPhyHelper phy;
+  phy.SetChannel (channel.Create ());
+
+  WifiMacHelper mac;
+  Ssid ssid = Ssid ("ns-3-ssid");
+
+  WifiHelper wifi;
+
+  NetDeviceContainer staDevices;
+  mac.SetType ("ns3::StaWifiMac",
+               "Ssid", SsidValue (ssid),
+               "ActiveProbing", BooleanValue (false));
+  staDevices = wifi.Install (phy, mac, wifiStaNodes);
+
+  NetDeviceContainer apDevices;
+  mac.SetType ("ns3::ApWifiMac",
+               "Ssid", SsidValue (ssid));
+  apDevices = wifi.Install (phy, mac, wifiApNode);
+
+  MobilityHelper mobility;
+
+  mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
+                                 "MinX", DoubleValue (0.0),
+                                 "MinY", DoubleValue (0.0),
+                                 "DeltaX", DoubleValue (5.0),
+                                 "DeltaY", DoubleValue (10.0),
+                                 "GridWidth", UintegerValue (3),
+                                 "LayoutType", StringValue ("RowFirst"));
+
+  mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
+                             "Bounds", RectangleValue (Rectangle (-50, 50, -50, 50)));
+  mobility.Install (wifiStaNodes);
+
+  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  mobility.Install (wifiApNode);
+
+  Ipv6ListRoutingHelper listRh;
+  Ipv6StaticRoutingHelper staticRh;
+  listRh.Add(staticRh, 1);
+
+  InternetStackHelper stack;
+  stack.SetIpv4StackInstall(false); // IPv6 only
+  stack.SetRoutingHelper(listRh);
+  stack.Install(wifiStaNodes);
+
+  Ipv6AddressHelper address;
+  address.SetBase(Ipv6Address("2001:1::"), Ipv6Prefix(64));
+
+  Ipv6InterfaceContainer staInterfaces = address.Assign (staDevices);
+  address.Assign (apDevices);
+
+  UdpEchoServerHelper echoServer (9);
+
+  ApplicationContainer serverApps = echoServer.Install (wifiStaNodes.Get (0));
+  serverApps.Start (Seconds (1.0));
+  serverApps.Stop (Seconds (10.0));
+
+  UdpEchoClientHelper echoClient (staInterfaces.GetAddress (0, 1), 9);
+  echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
+  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
+  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+
+  ApplicationContainer clientApps = echoClient.Install(wifiApNode);
+  clientApps.Start (Seconds (2.0));
+  clientApps.Stop (Seconds (10.0));
+
+  Simulator::Stop (Seconds (10.0));
+
+  if (tracing)
+    {
+      phy.SetPcapDataLinkType (WifiPhyHelper::DLT_IEEE802_11_RADIO);
+      phy.EnablePcap ("third", apDevices.Get (0));
+    }
+
+  std::ostringstream oss;
+  oss << 
+    "/NodeList/" << wifiStaNodes.Get (nWifi - 1)->GetId ()  <<
+    "/$ns3::MobilityModel/CourseChange";
+  Config::Connect (oss.str (), MakeCallback (&CourseChange));
+  Simulator::Run ();
+  Simulator::Destroy ();
+  return 0;
+}
